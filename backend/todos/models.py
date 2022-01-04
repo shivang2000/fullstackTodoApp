9da6487bd1f5f70d7ed37f11827d7aa5d16e6230
shivang2000 <shivangchheda@gmail.com>
2022-01-05 05:09:45 +0530

    complete basic setup of full stack todo app with react and django,also add the todos models in todos app.

---
+++ backend/todos/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class ToDos(models.Model):
+    STATUSCHOICE = [
+        ( 'Pending', 'Pending'),
+        ( 'Completed', 'Completed'),
+    ]
+
+    whattodo = models.CharField(max_length=500, default="I got nothing to do")
+    status = models.CharField(max_length=20, choices=STATUSCHOICE, default=STATUSCHOICE[0][0])
+
+    def __str__(self) :
+        return str(self.whattodo)
