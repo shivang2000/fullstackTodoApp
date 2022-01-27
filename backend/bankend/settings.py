43222d047bda90f167a12be25b6740d14916621f
shivang2000 <shivangchheda@gmail.com>
2022-01-27 23:13:15 +0530

    added basic custom user

--- backend/bankend/settings.py
+++ backend/bankend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'todos',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.CustomUser'
